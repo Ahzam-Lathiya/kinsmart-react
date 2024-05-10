ac443ee0f72b22e6f4d2375b5038486b77fe9cba
ahzam_lathiya <ahzam.lathiya@gmail.com>
2024-05-10 18:38:42 +0500

    tic with x made

--- src/App.jsx
+++ src/App.jsx
@@ -3,33 +3,60 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+function Square(){
+  const [value, setValue] = useState(null);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+  function handleClick(){
+    setValue('X');
+  }
+
+  return <button onClick={handleClick} className="square">{value}</button>;
+}
+
+function App() {
+  const [count, setCount] = useState(0);
+  return(
+  <>
+  <div className='board-row'>
+    {/* <button className="square">X</button>
+    <button className="square">X</button>
+    <button className="square">X</button> */}
+    <Square value="1"/>
+    <Square value="2"/>
+    <Square value="3"/>
+  </div>
+  <div className='board-row'>
+    <Square value="4"/>
+    <Square value="5"/>
+    <Square value="6"/>
+  </div>
+  <div className='board-row'>
+    <Square value="7"/>
+    <Square value="8"/>
+    <Square value="9"/>
+  </div>
+    {/* //   <div>
+    //     <a href="https://vitejs.dev" target="_blank">
+    //       <img src={viteLogo} className="logo" alt="Vite logo" />
+    //     </a>
+    //     <a href="https://react.dev" target="_blank">
+    //       <img src={reactLogo} className="logo react" alt="React logo" />
+    //     </a>
+    //   </div>
+    //   <h1>Vite + React</h1>
+    //   <div className="card">
+    //     <button onClick={() => setCount((count) => count + 1)}>
+    //       count is {count}
+    //     </button>
+    //     <p>
+    //       Edit <code>src/App.jsx</code> and save to test HMR
+    //     </p>
+    //   </div>
+    //   <p className="read-the-docs">
+    //     Click on the Vite and React logos to learn more
+    //   </p> */}
     </>
-  )
+);
 }
 
 export default App
